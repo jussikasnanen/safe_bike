6b87e08d17df6163dc2cd757de4d37e1094214c1
Jussi Käsnänen <jussi.kasnanen@gmail.com>
2023-05-16 22:47:37 +0300

    Setup for dev env - dev env config file devbox.json add - install script for VS Code extensions

---
+++ install-vscode-extensions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+code --install-extension ionide.ionide-fsharp
+code --install-extension ms-dotnettools.csharp
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension usernamehw.errorlens
+code --install-extension fernandoescolar.vscode-solution-explorer
+code --install-extension jetpack-io.devbox
