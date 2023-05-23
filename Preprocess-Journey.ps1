cb337c2ca9955cc455e5991b16ce4c5510236338
Jussi Käsnänen <jussi.kasnanen@gmail.com>
2023-05-23 22:46:53 +0300

    Preprocessing of journey data with PowerShell script.

---
+++ Preprocess-Journey.ps1
@@ -0,0 +1,19 @@
+
+$FileName = 'output.csv'
+
+Import-CSV -Delimiter ',' 'datasets/journey/2021-05.csv' | ForEach-Object { 
+ 
+    #Current row object
+    $CSVRecord = $_
+        
+    #Read column values in the current row 
+    $Duration = $CSVRecord.'Duration (sec.)'
+    $Distance = $CSVRecord.'Covered distance (m)'
+
+    if ($Distance -lt 10 -and $Duration -lt 10) {
+        $CSVRecord | Export-Csv -Path $FileName -NoTypeInformation -Append
+    }
+
+    #if ($Distance -gt 10 -and $Duration -gt 10)
+        #$CSVRecord | Export-Csv -Path $FileName -NoTypeInformation -Append
+}
