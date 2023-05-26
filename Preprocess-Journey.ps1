e19ffb32b53441b6463ed49c533a6b52df0653aa
Jussi Käsnänen <jussi.kasnanen@gmail.com>
2023-05-26 16:36:21 +0300

    Preprocessing of journey data into intermediate files.

--- Preprocess-Journey.ps1
+++ Preprocess-Journey.ps1
@@ -1,19 +1,20 @@
+$FileName1 = 'output_journeys_too_short.csv'
+$FileName2 = 'output_journeys_dataok.csv'
 
-$FileName = 'output.csv'
-
-Import-CSV -Delimiter ',' 'datasets/journey/2021-05.csv' | ForEach-Object { 
+Import-CSV -Delimiter ',' 'datasets/journey/2021-05.csv' | ForEach-Object {
  
     #Current row object
     $CSVRecord = $_
-        
+
     #Read column values in the current row 
     $Duration = $CSVRecord.'Duration (sec.)'
     $Distance = $CSVRecord.'Covered distance (m)'
 
     if ($Distance -lt 10 -and $Duration -lt 10) {
-        $CSVRecord | Export-Csv -Path $FileName -NoTypeInformation -Append
+        $CSVRecord | Export-Csv -Path $FileName1 -NoTypeInformation -Append
     }
 
-    #if ($Distance -gt 10 -and $Duration -gt 10)
-        #$CSVRecord | Export-Csv -Path $FileName -NoTypeInformation -Append
+    if ($Distance -gt 10 -and $Duration -gt 10) {
+        $CSVRecord | Export-Csv -Path $FileName2 -NoTypeInformation -Append
+    }
 }
